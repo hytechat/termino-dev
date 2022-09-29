8bac2798bc13d9ea13f07bfdbc46c80d8807d88a
Nenad Milutinovic <fhb201257@fh-vie.ac.at>
2022-09-29 19:36:00 +0200

    Buttons auf Home Page eingefügt

--- frontend/lib/pages/pagesGeruest/views/home_page.dart
+++ frontend/lib/pages/pagesGeruest/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:termino_frontend/config/config_expo.dart';
 import 'package:termino_frontend/data/model/meeting_model.dart';
 import 'package:termino_frontend/data/repository/meeting_repository.dart';
@@ -23,25 +24,49 @@ class _HomePageState extends State<HomePage> {
     _fetchMeeting('666');
     return Container(
         color: Colourpalette.backgroundhellbeigeGrau,
-        child: Column(
-          children: [
-            IconsGoogle.personIcon,
-            basicButton(
-              'Abstimmung teilnehmen Test',
-              false,
-              () => showCupertinoModalPopup(
-                //whs nicht die beste variante um das aufzurufen
-                useRootNavigator: true,
-                barrierDismissible: false,
-                context: context,
-                builder: (_) => AbstimmugTeilnehmenPage(
-                  seitenTitel: 'Abstimmung Teilnehmen',
-                  geholtesMeeting: widget.geholtesMeeting,
-                ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+
+                  Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(20),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(50),
+                        ),
+                        child: const Text('Log In'),
+                        onPressed: () {},
+                      )),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'REGISTRIEREN',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ),                  
+                  Container(
+                      height: 80,
+                      padding: const EdgeInsets.all(20),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(50),
+                        ),
+                        child: const Text('Als Gast Fortfahren'),
+                        onPressed: () {},
+                      )),
+
+                ],
               ),
-            ),
-          ],
-        ));
+            );
   }
 //--------------------------api shiat zum aufrufen der termin teilnehmen page------------------------------------
 
