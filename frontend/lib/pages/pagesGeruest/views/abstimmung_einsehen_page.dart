5e7fd6c37419326eb5f7e847e7c9f08a5fe97bb2
jakobdinhof <80478772+jakobdinhof@users.noreply.github.com>
2022-12-03 16:36:51 +0100

    Second Draft

--- frontend/lib/pages/pagesGeruest/views/abstimmung_einsehen_page.dart
+++ frontend/lib/pages/pagesGeruest/views/abstimmung_einsehen_page.dart
@@ -2,47 +2,113 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:termino_frontend/pages/pagesGeruest/profile_details/appbar_widget.dart';
+import 'package:termino_frontend/pages/pagesGeruest/profile_details/user_data.dart';
 
 class AbstimmungEinsehenPage extends StatefulWidget {
+  const AbstimmungEinsehenPage({Key? key}) : super(key: key);
+
   @override
   _AbstimmungEinsehenPage createState() => _AbstimmungEinsehenPage();
 }
 
 class _AbstimmungEinsehenPage extends State<AbstimmungEinsehenPage> {
+      var user = UserData.myUser;
+  
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Form(
-          child: Column(
-            children: [
-              SizedBox(height: height * 0.25),
-              const Text(
-                "Test",
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: height * 0.05,
-              ),
-            ],
+      appBar: buildAppBar(context),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget> [
+        const SizedBox(
+          width: 300,
+          child: Text(
+            "Abstimmung einsehen",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold
+            ),
           ),
         ),
-      ),
-    );
+       Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.98,
+              child: TextFormField(
+                decoration: const InputDecoration(labelText: 'Titel des Termins'),
+                initialValue: 'Weihnachten',
+                enabled: false,
+              ),
+            ),
+          ),
+        ],
+       ),
+       Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.98,
+              child: TextFormField(
+                decoration: const InputDecoration(labelText: 'Ort'),
+                initialValue: 'Hauptbahnhof',
+                enabled: false,
+              ),
+            ),
+          ),
+        ],
+       ),
+       Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.98,
+              child: TextFormField(
+                decoration: const InputDecoration(labelText: 'Organisator'),
+                initialValue: 'Markus',
+                enabled: false,
+              ),
+            ),
+          ),
+        ],
+       ),
+       Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.55,
+              width: MediaQuery.of(context).size.width * 0.98,
+              child: DecoratedBox(
+                decoration: const BoxDecoration(
+                  color: Colors.grey
+                  )),
+            ),
+          )
+        ],
+       )
+      ],
+    ));
   }
 
   FutureOr onGoBack(dynamic value) {
     setState(() {});
   }
 
-void navigateSecondPage(Widget editForm) {
+  void navigateSecondPage(Widget editForm) {
     Route route = MaterialPageRoute(builder: (context) => editForm);
     Navigator.push(context, route).then(onGoBack);
   }
-
 }
-
-
